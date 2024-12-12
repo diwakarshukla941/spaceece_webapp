a15a4ccd6e19b01e6767e1409e1c2248b9a76bc3
rajdahare <138197873+rajdahare@users.noreply.github.com>
2024-12-12 13:26:24 +0530

    Update login_controller.php

---
+++ controllers/login_controller.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include('../connect.php');
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+
+$check = mysqli_query($connect, "SELECT * FROM user WHERE email='$email' AND password='$password' ");
+
+if(mysqli_num_rows($check)>0){
+
+    $parentsdata = mysqli_fetch_array($check);
+    $_SESSION['parentsdata'] = $parentsdata;
+
+    if($check){
+        echo '
+            <script>
+                window.location = "../views/main.php";
+            </script>
+        ';
+    }
+}else{
+    echo '
+        // <script>
+        //     alert("Invalid credentials or user not found!");
+        //     window.location = "../";
+        // </script>
+    ';
+}
+
+?>
